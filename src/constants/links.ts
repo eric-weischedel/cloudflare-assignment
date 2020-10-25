2305d3eef5fd3d0d8b82cbf70558efae1f254144
Eric Weischedel <eweischedel@scheduleengine.com>
2020-10-25 11:55:00 -0400

    Create links endpoint

---
+++ src/constants/links.ts
@@ -0,0 +1,25 @@
+interface Link {
+  name: string;
+  url: string;
+}
+
+const links: Link[] = [
+  {
+    name: 'Netflix',
+    url: 'https://netflix.com',
+  },
+  {
+    name: 'Disney+',
+    url: 'https://disneyplus.com',
+  },
+  {
+    name: 'Peacock',
+    url: 'https://peacocktv.com',
+  },
+  {
+    name: 'Crunchyroll',
+    url: 'https://crunchyroll.com',
+  },
+];
+
+export default links;
--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,18 @@
+import links from './constants/links';
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const endpoint = request.url.split('/').pop();
+
+  switch (endpoint) {
+    case 'links':
+      return new Response(JSON.stringify(links, null, 2), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    default:
+      return new Response(null, {
+        status: 404,
+      });
+  }
 }
